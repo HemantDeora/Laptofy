ae90fe059a4befdfdf063985ccea39a3367f8be6
Hemant deora <hemantdeora923@gmail.com>
2026-02-06 18:56:27 +0530

    switced to loacldata

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
 
 
     <footer className="bg-yellow-500 dark:bg-gray-900">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+      <div className="mx-auto w-full max-w-7xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
             <a href="https://v3.flowbite.com/" className="flex items-center">
